ff8c8699287c01239bea21ed94e5b2c11c5cba23
azizbekhafizov <azizbekhafizov678@gmail.com>
2025-01-08 13:26:18 +0500

    new

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let menu_icon = document.querySelector('.menu_icon')
 let sidebar = document.querySelector('.sidebar')
 let sidebar_icon = document.querySelector('.sidebar_icon')
+const loadingSpinner = document.getElementById('loading-spinner');
 
 menu_icon.addEventListener('click', () => {
     sidebar.classList.add('open');
@@ -10,3 +11,8 @@ sidebar_icon.addEventListener('click', () => {
     sidebar.classList.remove('open')
 });
 
+
+loadingSpinner.style.display = 'block';
+setTimeout(() => {
+  loadingSpinner.style.display = 'none';
+}, 2000);
